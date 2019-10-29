56ecfc355a65f3147a235337b75558adf0afeb04
tianyu <tianyu5177@sina.com>
2019-10-29 10:44:15 +0800

    update

--- day02/2.Node中的文件操作(写入)/1.简单文件写入.js
+++ day02/2.Node中的文件操作(写入)/1.简单文件写入.js
@@ -1,6 +1,29 @@
 /*
-* Node中的文件系统：
+*1. Node中的文件系统：
 *     1.在NodeJs中有一个文件系统，所谓的文件系统，就是对计算机中的文件进行增删改查等操作。
 *     2.在NodeJs中，给我们提供了一个模块，叫做fs模块(文件系统)，专门用于操作文件。
 *     3.fs模块是Node的核心模块，使用的时候，无需下载，直接引入。
+*
+*2.异步文件写入 （简单文件写入）
+*   fs.writeFile(file, data[, options], callback)
+*           --file：要写入的文件路径+文件名+后缀
+*           --data：要写入的数据
+*           --options：可选参数
+*           --callback：回调函数
+*                 --err：错误对象
+*
+*  在Node中有这样一个原则：错误优先
 */
+
+//引入内置的fs模块
+let fs = require('fs')
+
+//调用writeFile方法
+fs.writeFile('./demo.txt','美女!',(err)=>{
+    if(err){
+      console.log('文件写入失败',err)
+    }else{
+      console.log('文件写入成功')
+    }
+})
+
