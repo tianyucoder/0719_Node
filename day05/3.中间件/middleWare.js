197cb675aa5182a380a64044509f73cc2ea73805
tianyu <tianyu5177@sina.com>
2019-11-02 15:01:50 +0800

    update

--- day05/3.中间件/middleWare.js
+++ day05/3.中间件/middleWare.js
@@ -4,7 +4,7 @@
 
  作用：
         1)	执行任何代码。
-        2)	修改请求和响应对象。
+        2)	修改请求对象、响应对象。
         3)	终结请求-响应循环。(让一次请求得到响应)
         4)	调用堆栈中的下一个中间件或路由。
   分类：
@@ -21,6 +21,68 @@
    备注：
         1.在express中，定义路由和中间件的时候，根据定义的顺序（代码的顺序），将定义的每一个中间件或路由，
         放在一个类似于数组的容器中，当请求过来的时候，依次从容器中取出中间件和路由，进行匹配，如果匹配
-        成功，交由该路由或中间件处理。
+        成功，交由该路由或中间件处理，如果全局中间件写在了最开始的位置，那么他就是请求的“第一扇门”。
         2.对于服务器来说，一次请求，只有一个请求对象，和一个响应对象，其他任何的request和response都是对二者的引用。
  */
+
+const express = require('express')
+
+const app = express()
+
+//【第一种】使用应用级(全局)中间件------所有请求的第一扇门
+/*app.use((request,response,next)=>{
+  //response.send('我是中间件给你的响应')
+  //response.test = 1 //修改请求对象
+  //图片防盗链
+  if(request.get('Referer')){
+    let miniReferer = request.get('Referer').split('/')[2]
+    if(miniReferer === 'localhost:63347'){
+      next()
+    }else{
+      //发生了盗链
+      response.sendFile(__dirname+'/public/err.png')
+    }
+  }else{
+    next()
+  }
+  //next()
+})*/
+
+//第二种使用全局中间件的方式------更加灵活，不是第一扇门，可以在任何需要的地方使用
+function guardPic(request,response,next) {
+  //防盗链
+  if(request.get('Referer')){
+    let miniReferer = request.get('Referer').split('/')[2]
+    if(miniReferer === 'localhost:63347'){
+      next()
+    }else{
+      //发生了盗链
+      response.sendFile(__dirname+'/public/err.png')
+    }
+  }else{
+    next()
+  }
+}
+
+app.get('/',(request,response)=>{
+    console.log(request.demo)
+    response.send('ok')
+})
+
+app.get('/demo',(request,response)=>{
+    console.log(request.demo)
+    console.log(request.query)
+    response.send('ok2')
+})
+
+app.get('/picture',guardPic,(request,response)=>{
+  console.log(request.demo)
+  response.sendFile(__dirname+'/public/demo.jpg')
+})
+
+
+
+app.listen(3000,function (err) {
+  if(!err) console.log('ok')
+  else console.log(err)
+})
