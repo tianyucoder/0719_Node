adeaf2b308f52a274b9a6b4870fd7fb3907d02a8
tianyu <tianyu5177@sina.com>
2019-11-05 17:03:45 +0800

    update

--- Ajax/1.原生ajax/server.js
+++ Ajax/1.原生ajax/server.js
@@ -1,18 +1,29 @@
 let express = require('express')
 
 let app = express()
+app.use(express.urlencoded({extended:true}))
 
 //暴露静态资源
 app.use(express.static(__dirname+'/public'))
 
 app.get('/ajax_get',function (req,res) {
+  console.log('有人发了get请求给我')
+  console.log(req.query)
   res.send('你发来的是get请求，我收到了')
+})
+
+app.post('/ajax_post',function (req,res) {
+  console.log('有人发了post请求给我')
+  console.log(req.body)
+  res.send('你发来的是post请求，我收到了')
 })
 
 app.listen(3000,function (err) {
   if(err) console.log(err)
   else {
-    console.log('兄弟，别用编译器打开网页，会产生跨域问题！！！')
-    console.log('点击这里去练习吧：http://localhost:3000/ajax_get.html')
+    console.log('【练习原生js发送ajax服务器启动成功！】')
+    console.log('【兄弟，别用编译器打开网页，会产生跨域问题！！！】')
+    console.log('【点击这里去练习吧：http://localhost:3000/ajax_get.html】')
+    console.log('【点击这里去练习吧：http://localhost:3000/ajax_post.html】')
   }
 })
