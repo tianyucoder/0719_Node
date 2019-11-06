114056ff822af817a5b67db491f0fcbed5369c3a
tianyu <tianyu5177@sina.com>
2019-11-06 16:52:45 +0800

    update

--- Ajax/7.三级联动菜单/server.js
+++ Ajax/7.三级联动菜单/server.js
@@ -8,6 +8,7 @@ db(()=>{
   //获取中国所有省份信息
   app.get('/get_all_province',function (req,res) {
     citiesModel.find({level:1},{province:1,name:1,_id:0},function (err,data) {
+      res.setHeader('Access-Control-Allow-Origin','*')
       if(!err && data){
         res.send({status:1,data})
       }else{
@@ -18,6 +19,7 @@ db(()=>{
 
   //根据【省份编码】，获取某省份下的所有市信息
   app.get('/get_cities_by_province',function (req,res) {
+    res.setHeader('Access-Control-Allow-Origin','*')
     const {province} = req.query
     citiesModel.find({level:2,province},{city:1,name:1,_id:0},function (err,data) {
       if(!err && data){
@@ -30,6 +32,7 @@ db(()=>{
 
   //根据【省份编码】，【市编码】获取某省份下某市下的所有区县信息
   app.get('/get_counties_by_province_and_city',function (req,res) {
+    res.setHeader('Access-Control-Allow-Origin','*')
     const {province,city} = req.query
     citiesModel.find({level:3,province,city},{county:1,name:1,_id:0},function (err,data) {
       if(!err && data){
